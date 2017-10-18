a6a65c3dfdeaeb8e74f6f7d79a6740e0963e54f5
Sulabh <sulabh4@hotmail.com>
2017-10-18 09:26:43 +0545

    Mail SMTP Configured using OO Conecpt

--- contact.php
+++ contact.php
@@ -221,9 +221,7 @@ textarea.form-control{
 </html>
 
 <?php
-//Abp4952Bfj1069
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+require_once('functions/MailSender.php');
 
 if (isset($_POST['submit'])){
 
@@ -231,70 +229,14 @@ if (isset($_POST['submit'])){
     $name = $_POST['nameField'];
     $subject = $_POST['subjectField'];
     $description = $_POST['messageField'];
-    
-    //Load composer's autoloader
-    require 'vendor/autoload.php';
-    $mail = new PHPMailer(true);       
-    $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-    $mail->Port = 587;
-    $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'lynxcorporative@gmail.com';                 // SMTP username
-    $mail->Password = 'Abp4952Bfj1069';                           // SMTP password
-    $mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl' also accepted
-    
-    $mail->From = 'lynxcorporative@gmail.com';
-    $mail->FromName = 'Lynx Corporative';
-    $mail->addAddress($email, $name);     // Add a recipient
-    // $mail->addAddress('ellen@example.com');               // Name is optional
-    // $mail->addReplyTo('info@example.com', 'Information');
-    $mail->addCC('lynxcorporative@gmail.com');
-    // $mail->addBCC('bcc@example.com');
-    
-    $mail->WordWrap = 50;                                 // Set word wrap to 50 characters
-    // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-    $mail->isHTML(true);                                  // Set email format to HTML
-    
-    $headers = "MIME-Version: 1.0\r\n";
-    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    $message = '<html><body>';
-    $message .= '<img src="https://wallpaperscraft.com/image/couple_food_table_coffee_shop_fast_food_57082_3840x2400.jpg" height="500" width="800" />';
-    $message .= '<br />';
-    $message .= '<h1 style="color:brown;text-align:center;font-family:Comic Sans MS">Lynx Corporative</h1>';
-    $message .= '<br />';
-    $message .= '<p style="text-align:center;font-family:Comic Sans MS">We are delighted to have a question from you. We will reply ASAP. Thankyou.</p>';
-    $message .= '<p style="text-align:center;font-family:Comic Sans MS">Your Question.</p>';
-    $message .= '<p style="text-align:center;font-family:Comic Sans MS">' . $description . '</p>';
-    $message .= '<br />';
-    $message .= '<br />';
-    $message .= '<table align="center" width="604" cellpadding="5" cellspacing="0"> <tr> <td width="288" bgcolor="#ffffff"><img src="https://cdn3.iconfinder.com/data/icons/free-social-icons/67/facebook_square-512.png" height="35" width="35" /><br /> <td width="294" bgcolor="#ffffff" align="right"> <img src="http://icons.iconarchive.com/icons/uiconstock/socialmedia/512/Twitter-icon.png" height="35" width="35" /><tr>';
-    $message .= '<td style="background-color: #ffffff; border-top: 0px solid #000000; text-align: left; height: 50px;" align="center">
-<span style="font-size: 10px; color: #575757; line-height: 120%; font-family: arial; text-decoration: none;">
-<a href="mailto:info@petanthology.com">
-Contact Us?</a><br>
-Visit us on the web at <a href="default.asp">petanthology.com</a></span></td>
+    $emailProcess = new MailSender($email, $name, $subject, $description);
+    $emailProcess -> setEmail($email);
+    $emailProcess -> setName($name);
+    $emailProcess -> setSubject($subject);
+    $emailProcess -> setDescription($description);
+    $emailProcess -> sendMail();
 
-<td style="background-color: #ffffff; border-top: 0px solid #000000; text-align: right; height: 50px;" align="center">
-<span style="font-size: 10px; color: #575757; line-height: 120%;
-font-family: arial; text-decoration: none;">If you no longer want to receive our emails, please <a href="default.asp">un-subscribe here</a>.</span>';
-    $message .= '</table>';
-    $message .= "</body></html>";
-
-    $mail->Subject = $subject;
-    $mail->Header = $headers;
-    $mail->Body = $message;
-    // $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-    
-    if(!$mail->send()) {
-        echo 'Message could not be sent.';
-        echo 'Mailer Error: ' . $mail->ErrorInfo;
-    } else {
-        echo 'Message has been sent';
-    }
-    
 }
 
 ?>
