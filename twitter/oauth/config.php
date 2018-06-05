88798c672a4cbe414f699b325a9f36567fbbbd00
Sulabh Shrestha <tsulabh4@gmail.com>
2018-06-05 08:06:10 +0545

    Twitter Delete

--- twitter/oauth/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('CONSUMER_KEY', 'aEmzmDkEgG1QkPWavWNQ4eyx5'); // YOUR CONSUMER KEY
-define('CONSUMER_SECRET', 'DXcM21ElW348RGLA3PvanY3k2DBVzZH95xQFlLQ8U6P335YbH1'); //YOUR CONSUMER SECRET KEY 
-define('OAUTH_CALLBACK', 'http://localhost/lynxco/twitter/oauth/index.php');  // Redirect URL 
-?>
